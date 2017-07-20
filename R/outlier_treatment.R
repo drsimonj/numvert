bdae89408f548264b2aa0f1bb854613e559204ed
Simon Jackson <drsimonjackson@gmail.com>
2017-07-20 21:24:51 +0200

    Add: functions to truncate

--- R/outlier_treatment.R
+++ R/outlier_treatment.R
@@ -1,34 +1,39 @@
 #
 #
-# Functions that treats outliers
+# Functions that treat outliers
 #
 #
 
-#' Cap a numeric vector at a specified value
+#' Truncate numeric vector at specified value(s)
 #'
-#' Replace all elements of a numeric vector that are above a specified value
-#' with another value
-#'
-#' @export
-#' @param x Numeric vector
-#' @param cap Value above which to replace
-#' @param replacement Value with which to replace elements above cap
-cap_at <- function(x, cap, replacement = NA) {
-  x[x > cap] <- replacement
-  x
-}
-
-#' Floor a numeric vector at a specified value
-#'
-#' Replace all elements of a numeric vector that are below a specified value
-#' with another value
+#' Replace elements of a numeric vector identified by specific cut point(s)
 #'
 #' @export
 #' @param x Numeric vector
-#' @param floor Value below which to replace
-#' @param replacement Value with which to replace elements below floor
-floor_at <- function(x, floor, replacement = NA) {
-  x[x < floor] <- replacement
+#' @param below Value below which to truncate
+#' @param above Value above which to truncate
+#'
+#' @examples
+#' num_truncate(0:10, 2, 8)
+#' num_truncate_below(0:10, 2)
+#' num_truncate_above(0:10, 8)
+#'
+num_truncate <- function(x, below = -Inf, above = Inf) {
+  x <- num_truncate_below(x, below)
+  x <- num_truncate_above(x, above)
   x
 }
 
+#' @rdname num_truncate
+#' @export
+num_truncate_below <- function(x, below) {
+  x[x < below] <- NA
+  x
+}
+
+#' @rdname num_truncate
+#' @export
+num_truncate_above <- function(x, above) {
+  x[x > above] <- NA
+  x
+}
