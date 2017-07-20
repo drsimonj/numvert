d18a7fb417213084d285141b1331a8110dc3eaf2
Simon Jackson <drsimonjackson@gmail.com>
2017-07-20 22:11:13 +0200

    Change: truncate functionality to use proportions

--- R/outlier_treatment.R
+++ R/outlier_treatment.R
@@ -4,36 +4,31 @@
 #
 #
 
-#' Truncate numeric vector at specified value(s)
+#' Truncate proportion of numeric vector
 #'
-#' Replace elements of a numeric vector identified by specific cut point(s)
+#' Truncate a proportion of elements from the top, bottom, or both tails of a
+#' numeric vector by replacing them with NA
 #'
 #' @export
 #' @param x Numeric vector
-#' @param below Value below which to truncate
-#' @param above Value above which to truncate
+#' @param prop Proportion of data to truncate
+#' @param tail Tail(s) to truncate
 #'
 #' @examples
-#' num_truncate(0:10, 2, 8)
-#' num_truncate_below(0:10, 2)
-#' num_truncate_above(0:10, 8)
-#'
-num_truncate <- function(x, below = -Inf, above = Inf) {
-  x <- num_truncate_below(x, below)
-  x <- num_truncate_above(x, above)
-  x
-}
+#' num_trunc(0:10, 2, 8)
+num_trunc <- function(x, prop, tail = c("both", "top", "bottom")) {
+  tail <- match.arg(tail)
+  if(prop <= 0 | prop >= 1)
+    stop("`prop` must be a value between 0 and 1")
 
-#' @rdname num_truncate
-#' @export
-num_truncate_below <- function(x, below) {
-  x[x < below] <- NA
-  x
-}
+  props <- switch(tail,
+    both   = c(prop/2, 1 - prop/2),
+    top    = c(0, 1 - prop),
+    bottom = c(prop, 1)
+  )
 
-#' @rdname num_truncate
-#' @export
-num_truncate_above <- function(x, above) {
-  x[x > above] <- NA
+  trunc_vals <- quantile(x, props, na.rm = TRUE)
+
+  x[x < trunc_vals[1] | x > trunc_vals[2]] <- NA
   x
 }
