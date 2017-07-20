b3179eec31de3b2b9988349be42484c928915417
Simon Jackson <drsimonjackson@gmail.com>
2017-07-20 21:26:43 +0200

    Change: naming of scale functions
    To gel with idea of prefixing functions with 'num_' , all function names updated.

--- R/scale.R
+++ R/scale.R
@@ -4,24 +4,17 @@
 #
 #
 
-
 #' Standardize a numeric vector
 #'
 #' @export
 #' @param x Numeric vector
 #' @param new_mean Numeric value of new mean
 #' @param new_sd Numeric value of new standard deviation
-standardize <- function(x, new_mean = 0, new_sd = 1) {
+num_std <- function(x, new_mean = 0, new_sd = 1) {
   x_mean <- mean(x, na.rm = TRUE)
   x_sd   <- sd(x, na.rm = TRUE)
 
   (x - x_mean) / x_sd * new_sd + new_mean
-}
-
-#' @export
-#' @rdname standardize
-standardise <- function(...) {
-  standardize(...)
 }
 
 #' Rescale a numeric vector
@@ -30,7 +23,7 @@ standardise <- function(...) {
 #' @param x Numeric vector
 #' @param new_min Numeric value of new minimum
 #' @param new_max Numeric value of new maximum
-rescale <- function(x, new_min = 0, new_max = 1) {
+num_rescale <- function(x, new_min = 0, new_max = 1) {
   if (new_max <= new_min)
     warning("`new_max` is not greater than `new_min`")
 
@@ -47,12 +40,6 @@ rescale <- function(x, new_min = 0, new_max = 1) {
 #'
 #' @export
 #' @inheritParams rescale
-normalize <- function(x) {
+num_norm <- function(x) {
   rescale(x, new_min = 0, new_max = 1)
-}
-
-#' @export
-#' @rdname normalize
-normalise <- function(...) {
-  normalize(...)
 }
