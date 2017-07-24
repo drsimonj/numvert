f85f1a92771bc8a5aff2a9fe59917ee163eda750
Simon Jackson <drsimonjackson@gmail.com>
2017-07-24 10:21:59 +0200

    Add: num_quantile() to locate quantiles

--- R/locations.R
+++ R/locations.R
@@ -4,3 +4,14 @@
 #
 #
 
+#' Locate quantile boundaries of a numeric vector
+#'
+#' \code{num_quantiles is a wrapper for \code{\link[stats]{quantile}} that
+#' sets the default values slightly differently
+#'
+#' @export
+#' @inheritParams stats::quantile
+#'
+num_quantile <- function(x, probs = c(0, 1), na.rm = TRUE, names = FALSE, ...) {
+  quantile(x, probs, na.rm = na.rm, names = names, ...)
+}
