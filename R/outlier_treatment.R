a26e6993e7ebedc8005c71a5f29a5d1cb5f70375
Simon Jackson <drsimonjackson@gmail.com>
2017-07-24 10:35:41 +0200

    Change: use explicit bounds for outlier detection functions
    This commit changed the style of outlier detection functions from providing a probability to providing explicit boundary values. The new approach is more flexible and allows other functions (like num_quantile) to handle the probability approach

--- R/outlier_treatment.R
+++ R/outlier_treatment.R
@@ -4,85 +4,86 @@
 #
 #
 
-#' Truncate proportion of numeric vector
+#' Truncate numeric vector
 #'
-#' Truncate a proportion of elements from the top, bottom, or both tails of a
-#' numeric vector by replacing them with NA
-#'
-#' @export
-#' @param x Numeric vector
-#' @param prop Proportion of data to target
-#' @param tail Tail(s) to target
-#'
-#' @examples
-#' num_truncate(0:10, .4)
-#' num_truncate(0:10, .4, "top)
-num_truncate <- function(x, prop, tail = c("both", "top", "bottom")) {
-  props <- p_to_quantiles(prop, tail)
-  taget_vals <- quantile(x, props, na.rm = TRUE)
-
-  x[x < taget_vals[1] | x > taget_vals[2]] <- NA
-  x
-}
-
-#' Truncate numeric vector using interquartile range
-#'
-#' Truncate elements of a numeric vector that fall outside the range defined by
-#' the vector's mean, plus/minus the interquartile range (IQR) multipled by some
-#' value (1.5 by default)
+#' Truncate top and/or bottom of a numeric vector by replacing with NA
 #'
 #' @export
 #' @param x Numeric vector
-#' @param iqr_multipler Value by which to multiply the IQR when calculating the
-#'   target boundary
+#' @param bounds Two-element numeric vector defining the lower and upper bounds
+#'   (in that order)
 #'
 #' @examples
-#' boxplot(iris$Sepal.Width)  # Outliers present
-#' boxplot(num_truncate_iqr(iris$Sepal.Width))  # Outliers removed
-#'
-num_truncate_iqr <- function(x, iqr_multipler = 1.5) {
-  if (iqr_multipler <= 0)
-    stop("`iqr_multipler` must be a value greater than zero")
+#' num_truncate(0:10, c(2, 8))
+num_truncate <- function(x, bounds = c(-Inf, Inf)) {
+  if (length(bounds) != 2)
+      stop("`bounds` must be a numeric vector with two elements")
+  if(bounds[1] >= bounds[2])
+    stop("The first number in `bounds` must be lower than the second")
 
-  qs <- quantile(x, c(.25, .75), na.rm = TRUE)
-  weighted_iqr <- iqr_multipler * (qs[2] - qs[1])
-  x_mean <- mean(x, na.rm = TRUE)
-
-  x[x < (x_mean - weighted_iqr) | x > (x_mean + weighted_iqr)] <- NA
+  x[x < bounds[1] | x > bounds[2]] <- NA
   x
 }
 
-#' Censor proportion of a numeric vector
+#' #' Truncate numeric vector using interquartile range
+#' #'
+#' #' Truncate elements of a numeric vector that fall outside the range defined by
+#' #' the vector's mean, plus/minus the interquartile range (IQR) multipled by some
+#' #' value (1.5 by default)
+#' #'
+#' #' @export
+#' #' @param x Numeric vector
+#' #' @param iqr_multipler Value by which to multiply the IQR when calculating the
+#' #'   target boundary
+#' #'
+#' #' @examples
+#' #' boxplot(iris$Sepal.Width)  # Outliers present
+#' #' boxplot(num_truncate_iqr(iris$Sepal.Width))  # Outliers removed
+#' #'
+#' num_truncate_iqr <- function(x, iqr_multipler = 1.5) {
+#'   if (iqr_multipler <= 0)
+#'     stop("`iqr_multipler` must be a value greater than zero")
 #'
-#' Censor a proportion of elements from the top, bottom, or both tails of a
-#' numeric vector by replacing them with the boundary value(s)
+#'   qs <- quantile(x, c(.25, .75), na.rm = TRUE)
+#'   weighted_iqr <- iqr_multipler * (qs[2] - qs[1])
+#'   x_mean <- mean(x, na.rm = TRUE)
+#'
+#'   x[x < (x_mean - weighted_iqr) | x > (x_mean + weighted_iqr)] <- NA
+#'   x
+#' }
+
+#' Censor a numeric vector
+#'
+#' Censor top and/or bottom of a numeric vector by replacing with the boundary
+#' value(s)
 #'
 #' @export
 #' @inheritParams num_truncate
 #'
 #' @examples
-#' num_censor(0:10, .4)
-#' num_censor(0:10, .4, "top)
-num_censor <- function(x, prop, tail = c("both", "top", "bottom")) {
-  props <- p_to_quantiles(prop, tail)
-  taget_vals <- quantile(x, props, na.rm = TRUE)
+#' num_censor(0:10, c(2, 8))
+num_censor <- function(x, bounds = c(-Inf, Inf)) {
+  if (length(bounds) != 2)
+    stop("`bounds` must be a numeric vector with two elements")
+  if(bounds[1] >= bounds[2])
+    stop("The first number in `bounds` must be lower than the second")
 
-  x[x < taget_vals[1]] <- taget_vals[1]
-  x[x > taget_vals[2]] <- taget_vals[2]
+  x[x < bounds[1]] <- bounds[1]
+  x[x > bounds[2]] <- bounds[2]
   x
 }
 
 
-#' Convert single probability and tail method to two quantiles
-p_to_quantiles <- function(prop, tail = c("both", "top", "bottom")) {
-  tail <- match.arg(tail)
-
-  if(prop <= 0 | prop >= 1)
-    stop("`prop` must be a value between 0 and 1")
-
-  switch(tail,
-    both   = c(prop/2, 1 - prop/2),
-    top    = c(0, 1 - prop),
-    bottom = c(prop, 1)
-  )
-}
+#' #' Convert single probability and tail method to two quantiles
+#' p_to_quantiles <- function(prop, tail = c("both", "top", "bottom")) {
+#'   tail <- match.arg(tail)
+#'
+#'   if(prop <= 0 | prop >= 1)
+#'     stop("`prop` must be a value between 0 and 1")
+#'
+#'   switch(tail,
+#'     both   = c(prop/2, 1 - prop/2),
+#'     top    = c(0, 1 - prop),
+#'     bottom = c(prop, 1)
+#'   )
+#' }
